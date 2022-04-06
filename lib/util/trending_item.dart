965435d5925d0410dae7bc52a5aaf955f3ba45ce
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 09:21:48 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -79,4 +79,3 @@ class _TrendingItemState extends State<TrendingItem> {
                     top: 6.0,
                     left: 6.0,
                     child: Card(
-                      shape: RoundedRectangleBorder(
