cdf33d564a8c97d9e6d1f70cd02c22c7a4c635a4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 14:17:55 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -16,7 +16,7 @@ class TrendingItem extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _TrendingItemState createState() => _TrendingItem
+  _TrendingItemState createState() => _TrendingItemS
 
 
     
