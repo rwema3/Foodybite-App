be0b622c36008b7f9aa185add50c1831070b9d94
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 14:59:00 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -50,4 +50,5 @@ class _TrendingItemState extends State<TrendingItem> {
                     ),
                   ),
                   Positioned(
-                    top: 6.
+                    top: 6.0,
+                    
