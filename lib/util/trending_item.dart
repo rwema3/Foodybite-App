778f6d00884b96230a767d3b073d9ba5da78da66
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 09:20:21 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -115,4 +115,3 @@ class _TrendingItemState extends State<TrendingItem> {
               Padding(
                 padding: EdgeInsets.only(left: 15.0),
                 child: Container(
-                  width: MediaQuery.of(context).size.width,
