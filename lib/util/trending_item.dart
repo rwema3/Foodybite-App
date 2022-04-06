2010257463f71544b8580c8c1ff3740c0a0946a4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 11:16:43 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -36,7 +36,7 @@ class _TrendingItemState extends State<TrendingItem> {
               Stack(
                 children: <Widget>[
                   Container(
-                    height: MediaQuery.of(c
+                    height: MediaQuery.of(
                   
                     
                        
