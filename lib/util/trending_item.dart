6852d0a2ddc119aa1c94593f774f9631234d65f8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 11:27:21 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -29,7 +29,7 @@ class _TrendingItemState extends State<TrendingItem> {
         width: MediaQuery.of(context).size.width,
         child: Card(
           shape:
-              RoundedRe 
+              RoundedR 
           
            
              
