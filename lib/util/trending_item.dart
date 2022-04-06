56829110fd2eda2e837bae11cedcda8fd7f8071e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 14:20:22 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -19,7 +19,7 @@ class TrendingItem extends StatefulWidget {
   _TrendingItemState createState() => _TrendingItemState();
 }
 
-class _TrendingItemState extend  
+class _TrendingItemState extends  
   
      
      
