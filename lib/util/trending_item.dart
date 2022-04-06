3e46fb321a2cdd26c417c60f019f8fe7232aa1b0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-06 14:53:28 +0200

    Update trending_item.dart

--- lib/util/trending_item.dart
+++ lib/util/trending_item.dart
@@ -40,5 +40,5 @@ class _TrendingItemState extends State<TrendingItem> {
                     width: MediaQuery.of(context).size.width,
                     child: ClipRRect(
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(1
+                        topLeft: Radius.circular(10
                       
