580f76ef683a0ab53b7eeb14fa67406826339340
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-17 14:00:02 +0200

    class _MyAppState extends State<MyApp> {   @override

---
+++ lib/widgets/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foodybite/screens/main_screen.dart';
+import 'package:flutter_foodybite/util/const.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Constants.appName,
+      theme: Constants.lightTheme,
+      darkTheme: Constants.darkTheme,
+      home: MainScreen(),
+    );
+  }
+}
