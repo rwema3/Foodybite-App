59f2c6f89c7b43a1ae43b0c72d2e86d65175ae3a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-18 20:39:52 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -18,6 +18,8 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: Constants.appName,
       theme: Constants.lightTheme,
-
+      darkTheme: Constants.darkTheme,
+      home: MainScreen(),
+    );
   }
 }
